cdfb074070e38afba323b2aa43b987ac098627cc
ZHENG Raymond <rerezheng@gmail.com>
2023-11-24 11:59:02 +0100

    push commentaire

--- src/application/Hourglass.java
+++ src/application/Hourglass.java
@@ -2,12 +2,14 @@ package application;
 
 public class Hourglass {
     private int imageID;
+    // liste des png et l'ordre de rotation
     private static final String [] imageURL = {"start.png","synchroniser.png","passed.png"};
 
     public Hourglass(){
         this.imageID = 0;
     }
     public String nextImage(){
+        // renvois l'image suivante dans la liste
         this.imageID = (this.imageID +1) %3;
         return Hourglass.imageURL[this.imageID];
     }

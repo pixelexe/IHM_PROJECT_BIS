5e34c6bd485807a0a57c7573aa6bbaefda948fc5
Romain <romainjacob012@gmail.com>
2023-11-24 11:18:07 +0100

    Fusion

--- src/application/HeaderAbstract.java
+++ src/application/HeaderAbstract.java
@@ -167,6 +167,7 @@ public abstract class HeaderAbstract extends Application {
         return modeDayNightHeaderHBoxRight;
     }
 
+
     public HBox Header(){
 
         HBox sorbonne = new HBox();
